4f1eef9735e22910ec5cb4551e4fc6bdc3d168f3
guanig <guanig@61b5324c-07d3-11de-b5bf-434079a3519f>
2009-03-03 13:03:41 +0000

    refactored

--- trunk/src/com/cogniance/simpledb/util/SimpleDBObjectBuilder.java
+++ trunk/src/com/cogniance/simpledb/util/SimpleDBObjectBuilder.java
@@ -1,5 +1,6 @@
 package com.cogniance.simpledb.util;
 
+import java.beans.PropertyDescriptor;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.HashMap;
@@ -9,7 +10,6 @@ import java.util.Map;
 import javax.persistence.Column;
 
 import org.apache.commons.beanutils.PropertyUtils;
-import org.apache.commons.lang.StringUtils;
 
 import com.xerox.amazonws.sdb.ItemAttribute;
 
@@ -19,41 +19,37 @@ import com.xerox.amazonws.sdb.ItemAttribute;
 public class SimpleDBObjectBuilder {
 
     @SuppressWarnings("unchecked")
-    private static final Map<Class, Map<String, PropertyDescription>> cache = new HashMap<Class, Map<String, PropertyDescription>>();
+    private static final Map<Class, Map<String, PropertyDescriptor>> cache = new HashMap<Class, Map<String, PropertyDescriptor>>();
 
-    @SuppressWarnings("unchecked")
     public static <T> Object buildObject(Class<T> clazz, List<ItemAttribute> attrs) {
         try {
             Object newObj = clazz.newInstance();
-            Map<String, PropertyDescription> map = cache.get(clazz);
+            Map<String, PropertyDescriptor> map = cache.get(clazz);
             if (map != null) {
                 for (ItemAttribute attribute : attrs) {
-                    PropertyDescription pd = map.get(attribute.getName());
+                    PropertyDescriptor pd = map.get(attribute.getName());
                     if (pd != null) {
-                        Object value = convertValue(pd.getType(), attribute.getValue());
+                        Object value = convertValue(pd.getPropertyType(), attribute.getValue());
                         PropertyUtils.setProperty(newObj, pd.getName(), value);
                     }
                 }
             } else {
-                Map<String, PropertyDescription> newMap = new HashMap<String, PropertyDescription>();
-                for (Method method : clazz.getDeclaredMethods()) {
-                    String methodName = method.getName();
-                    Class methodType = method.getReturnType();
-                    if (methodName.startsWith("get")) {
-                        String propertyName = StringUtils.uncapitalize(methodName.substring(methodName.indexOf("get")
-                                + "get".length()));
-                        String itemName = propertyName;
-                        Annotation a = method.getAnnotation(Column.class);
-                        if (a != null) {
-                            itemName = ((Column) a).name();
-                        }
-                        for (ItemAttribute attribute : attrs) {
-                            if (attribute.getName().equals(itemName)) {
-                                Object value = convertValue(methodType, attribute.getValue());
-                                newMap.put(attribute.getName(), new PropertyDescription(propertyName, methodType));
-                                PropertyUtils.setProperty(newObj, propertyName, value);
-                                break;
-                            }
+                Map<String, PropertyDescriptor> newMap = new HashMap<String, PropertyDescriptor>();
+                PropertyDescriptor[] properties = PropertyUtils.getPropertyDescriptors(clazz);
+                for (PropertyDescriptor pd : properties) {
+                    Method getter = clazz.getMethod(pd.getReadMethod().getName());
+                    String propertyName = pd.getName();
+                    String itemName = propertyName;
+                    Annotation a = getter.getAnnotation(Column.class);
+                    if (a != null) {
+                        itemName = ((Column) a).name();
+                    }
+                    for (ItemAttribute attribute : attrs) {
+                        if (attribute.getName().equals(itemName)) {
+                            Object value = convertValue(getter.getReturnType(), attribute.getValue());
+                            newMap.put(itemName, pd);
+                            PropertyUtils.setProperty(newObj, propertyName, value);
+                            break;
                         }
                     }
                 }
@@ -73,27 +69,6 @@ public class SimpleDBObjectBuilder {
             return Boolean.parseBoolean(originalValue);
         } else {
             return originalValue;
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    private static class PropertyDescription {
-
-        private String name;
-
-        private Class type;
-
-        public String getName() {
-            return name;
-        }
-
-        public Class getType() {
-            return type;
-        }
-
-        public PropertyDescription(String name, Class type) {
-            this.name = name;
-            this.type = type;
         }
     }
 }
