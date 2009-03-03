c12ced519c5c7d0fdda15e43999975cda6a0ee83
guanig <guanig@61b5324c-07d3-11de-b5bf-434079a3519f>
2009-03-03 15:39:19 +0000

    version 0.3: save and delete entity methods added

--- src/com/cogniance/simpledb/util/SimpleDBObjectBuilder.java
+++ src/com/cogniance/simpledb/util/SimpleDBObjectBuilder.java
@@ -3,6 +3,7 @@ package com.cogniance.simpledb.util;
 import java.beans.PropertyDescriptor;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,7 +30,8 @@ public class SimpleDBObjectBuilder {
                 for (ItemAttribute attribute : attrs) {
                     PropertyDescriptor pd = map.get(attribute.getName());
                     if (pd != null) {
-                        Object value = convertValue(pd.getPropertyType(), attribute.getValue());
+                        Method getter = clazz.getMethod(pd.getReadMethod().getName());
+                        Object value = convertValue(getter.getReturnType(), attribute.getValue());
                         PropertyUtils.setProperty(newObj, pd.getName(), value);
                     }
                 }
@@ -56,6 +58,29 @@ public class SimpleDBObjectBuilder {
                 cache.put(clazz, newMap);
             }
             return newObj;
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static List<ItemAttribute> getItemAttributes(Object entity) {
+        Class clazz = entity.getClass();
+        List<ItemAttribute> attrs = new ArrayList<ItemAttribute>();
+        try {
+            PropertyDescriptor[] properties = PropertyUtils.getPropertyDescriptors(entity);
+            for (PropertyDescriptor pd : properties) {
+                Method getter = clazz.getMethod(pd.getReadMethod().getName());
+                String propertyName = pd.getName();
+                Annotation a = getter.getAnnotation(Column.class);
+                if (a != null) {
+                    String attrName = ((Column) a).name();
+                    attrs.add(new ItemAttribute(attrName, PropertyUtils.getProperty(entity, propertyName).toString(),
+                            true));
+                }
+
+            }
+            return attrs;
         } catch (Exception e) {
             throw new IllegalStateException(e);
         }
